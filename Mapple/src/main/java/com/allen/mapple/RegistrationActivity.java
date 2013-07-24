d31b14e587226d6271d1ca5110b37a6e1b43af56
Daniel Perez <tuvistavie@gmail.com>
2013-07-24 22:23:44 +0900

    Update views.

--- Mapple/src/main/java/com/allen/mapple/RegistrationActivity.java
+++ Mapple/src/main/java/com/allen/mapple/RegistrationActivity.java
@@ -1,8 +1,12 @@
 package com.allen.mapple;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -17,44 +21,66 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.URI;
 
 
 public class RegistrationActivity extends Activity {
 
+    private EditText emailText;
+    private EditText passwordText;
+    private Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_save);
+        setContentView(R.layout.activity_newlogin);
+
+        emailText = (EditText) findViewById(R.id.email_register_edit);
+        passwordText = (EditText) findViewById(R.id.password_register_edit);
+        loginButton = (Button) findViewById(R.id.do_register_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new RegisterTask().execute();
+            }
+        });
     }
 
-    private boolean register() {
-        String serverUrl = "http://localhost:3000/api/users";
-        int TIMEOUT_MILLISEC = 10000;  // = 10 seconds
-        HttpParams httpParams = new BasicHttpParams();
-        HttpConnectionParams.setConnectionTimeout(httpParams, TIMEOUT_MILLISEC);
-        HttpConnectionParams.setSoTimeout(httpParams, TIMEOUT_MILLISEC);
-        HttpClient client = new DefaultHttpClient(httpParams);
+    class RegisterTask extends AsyncTask<Void, Void, Void> {
 
-        HttpPost request = new HttpPost(serverUrl);
+        protected Void doInBackground(Void... params) {
+            int TIMEOUT_MILLISEC = 10000;  // = 10 seconds
+            HttpParams httpParams = new BasicHttpParams();
+            HttpConnectionParams.setConnectionTimeout(httpParams, TIMEOUT_MILLISEC);
+            HttpConnectionParams.setSoTimeout(httpParams, TIMEOUT_MILLISEC);
+            HttpClient client = new DefaultHttpClient(httpParams);
+            URI uri = URI.create("http://tuvistavie.com:3000/api/users");
+            HttpPost request = new HttpPost(uri);
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Accept", "application/json");
 
-        JSONObject json = new JSONObject();
-        byte[] message = new byte[1];
-        try {
-            json.put("user[email]", "foo");
-            json.put("user[password]", "foo");
-            message = json.toString().getBytes("UTF8");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            JSONObject json = new JSONObject();
+            byte[] message = new byte[1];
+            try {
+                JSONObject user = new JSONObject();
+                user.put("email", emailText.getText().toString());
+                user.put("password", passwordText.getText().toString());
+                json.put("user", user);
+                message = json.toString().getBytes("UTF8");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            request.setEntity(new ByteArrayEntity(message));
+            try {
+                HttpResponse response = client.execute(request);
+                Log.d("FOOBARFOO", response.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
         }
-        request.setEntity(new ByteArrayEntity(message));
-        try {
-            HttpResponse response = client.execute(request);
-            Log.d("FOOBARFOO", response.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return true;
     }
 }
